d023da0ad281cbf944a6985664b58c84b104f449
Karthick Ramjee <6506458+karthick03@users.noreply.github.com>
2021-11-12 10:05:48 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+ 
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
