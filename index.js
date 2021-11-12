7eb049b011f7a2ae43052e179918f1b071920b0b
Karthick Ramjee <6506458+karthick03@users.noreply.github.com>
2021-11-12 10:13:50 +0530

    Init commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.extended.json'));
 const middlewares = jsonServer.defaults();
  
 server.use(middlewares);
